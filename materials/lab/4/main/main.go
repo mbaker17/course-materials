7b2cc2d8a56522ac72d366f27db71e34526c4978
Matthew Baker <55414566+mbaker17@users.noreply.github.com>
2022-03-23 11:28:34 -0600

    finished

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -9,6 +9,10 @@ import (
 
 
 func main() {
+
+	fileServer := http.FileServer(http.Dir("index.html")) // New code
+    http.Handle("/yeet", fileServer) // New code
+
 	wyoassign.InitAssignments()
 	log.Println("starting API server")
 	//create a new router
@@ -20,8 +24,11 @@ func main() {
 	router.HandleFunc("/assignment/{id}", wyoassign.GetAssignment).Methods("GET")
 	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")		
 	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")	
-	// router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
+	router.HandleFunc("/assignment/{id}", wyoassign.UpdateAssignment).Methods("PUT")
 
+	router.HandleFunc("/classes", wyoassign.GetClasses).Methods("GET")
+	router.HandleFunc("/class", wyoassign.CreateClass).Methods("POST")
+	router.HandleFunc("/class/{id}", wyoassign.DeleteClass).Methods("DELETE")	
 	http.Handle("/", router)
 
 	//start and listen to requests
